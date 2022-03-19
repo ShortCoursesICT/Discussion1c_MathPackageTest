b54e6f104d849520cc8a08070860e5c362311321
ShortCoursesICT <ict.cet.shortcourses@gmail.com>
2022-03-19 11:07:03 +0800

    Change testing from G

--- Activity2_test.go
+++ Activity2_test.go
@@ -2,18 +2,13 @@ package main
 
 import (
 	"testing"
-
-	. "github.com/franela/goblin"
 )
 
 func TestMathFunc(t *testing.T) {
-	g := Goblin(t)
-
-	g.Describe("Activity 2", func() {
-		g.Assert(modValue(10.0, 1.0)).Equal(0.0)
-		g.Assert(modValue(10.0, 3.0)).Equal(1.0)
-		g.Assert(modValue(10.0, 4.0)).Equal(2.0)
-		g.Assert(modValue(10.0, 7.0)).Equal(3.0)
-		g.Assert(modValue(10.0, 6.0)).Equal(4.0)
-	})
+	value, div := 10.0, 7.0
+	got := modValue(value, div)
+	res := 3.0
+	if got != res {
+		t.Errorf("modValue() = %f; want %f", got, res)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module GoBaAct2
 
 go 1.18
-
-require github.com/franela/goblin v0.0.0-20211003143422-0a4f594942bf // indirect
